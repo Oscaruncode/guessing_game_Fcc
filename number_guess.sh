d1895c88991ada0c954e83f47d65c85a2936d6c0
Oscaruncode Fuentes Perez <oscarfuentesperezosp@gmail.com>
2024-03-16 21:27:55 +0000

    feat:Get username from DB and verify its pre register or no

--- number_guess.sh
+++ number_guess.sh
@@ -10,3 +10,17 @@ echo "Enter your username:"
 read USERNAME
 echo $USERNAME
 
+
+$GET_USERNAME=$($PSQL "SELECT username FROM players WHERE username=$USERNAME")
+
+if [[ -z $GET_USERNAME ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
+
+
+
+#GET_USER_INFO=$($PSQL "SELECT p.player_id,p.username,g.score FROM players AS p LEFT JOIN games AS g ON p.player_id=g.player_id WHERE p.username='$USERNAME'")
+
